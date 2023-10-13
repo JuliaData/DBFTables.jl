b09073fdf35c01c44fc1e1faa017481ea9692492
Martijn Visser <mgvisser@gmail.com>
2023-10-13 22:30:56 +0200

    go directly from bytes until 0 to Symbol

--- src/DBFTables.jl
+++ src/DBFTables.jl
@@ -192,8 +192,10 @@ end
 
 "Read a field descriptor from the stream, and create a FieldDescriptor struct"
 function read_dbf_field(io::IO)
-    field_name_raw = String(read!(io, Vector{UInt8}(undef, 11)))
-    field_name = Symbol(split(field_name_raw, '\0')[1])
+    field_name_bytes = read(io, 11)
+    i = findfirst(iszero, field_name_bytes)
+    n = i === nothing ? 11 : i - 1
+    field_name = Symbol(field_name_bytes[1:n])
     field_type = read(io, Char)
     skip(io, 4)  # skip
     field_len = read(io, UInt8)
